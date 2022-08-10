f2551b7aca3e222a6765e2255539c985de1872b9
IanMunna <ianmunna@icloud.com>
2022-08-10 11:19:30 -0300

    static & templates

--- BlogCoder/Blog/views.py
+++ BlogCoder/Blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import BusquedaPelicula, PeliculaF
+from .models import Pelicula
+from datetime import datetime
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'main.html')
+

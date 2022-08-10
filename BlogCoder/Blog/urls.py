00b2de894fb4678d119ee83a88f4ac695faad976
IanMunna <ianmunna@icloud.com>
2022-08-10 16:23:04 -0300

    urls2

--- BlogCoder/Blog/urls.py
+++ BlogCoder/Blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('crear_pelicula/', views.CrearPelicula.as_view(), name='crear_pelicula'),
     path('editar_pelicula/<int:pk>/', views.EditarPelicula.as_view(), name='editar_pelicula'),
     path('eliminar_pelicula/<int:pk>/', views.EliminarPelicula.as_view(), name='eliminar_pelicula'),
-    path('peliculas/<pk:int>/', views.MostrarPelicula.as_view(), name='mostrar_pelicula'),
+    path('peliculas/<int:pk>/', views.MostrarPelicula.as_view(), name='mostrar_pelicula'),
 ]

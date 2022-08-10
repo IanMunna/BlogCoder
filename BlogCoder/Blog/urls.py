9e8e391418600c9fc0a50afc857c17e19a2bba40
IanMunna <ianmunna@icloud.com>
2022-08-10 16:38:03 -0300

    list_peliculas

--- BlogCoder/Blog/urls.py
+++ BlogCoder/Blog/urls.py
@@ -5,7 +5,7 @@ from .views import inicio, sobre_mi
 urlpatterns = [
     path('', inicio, name= 'index'),
     path('about/', sobre_mi, name='sobre_mi'),
-    path('peliculas/', views.ListPeliculas.as_view(), name='listado_peliculas'),
+    path('peliculas/', views.ListPeliculas.as_view(), name='list_peliculas'),
     path('crear_pelicula/', views.CrearPelicula.as_view(), name='crear_pelicula'),
     path('editar_pelicula/<int:pk>/', views.EditarPelicula.as_view(), name='editar_pelicula'),
     path('eliminar_pelicula/<int:pk>/', views.EliminarPelicula.as_view(), name='eliminar_pelicula'),

8fe5a82ae047f84c0ad0333bd954699c05343fdb
IanMunna <ianmunna@icloud.com>
2022-08-10 16:02:02 -0300

    urls

--- BlogCoder/BlogCoder/wsgi.py
+++ BlogCoder/BlogCoder/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogCoder/BlogCoder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogCoder.settings')
 
 application = get_wsgi_application()
